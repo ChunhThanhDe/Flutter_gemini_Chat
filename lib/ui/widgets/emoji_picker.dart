5ec0f75c23d1771fedca5a65909c4650b03b6d46
ChunhThanhDe <chunhthanhde.dev@gmail.com>
2024-08-05 09:43:15 +0700

    :green_heart: fix button back and update emoji theme

--- lib/ui/widgets/emoji_picker.dart
+++ lib/ui/widgets/emoji_picker.dart
@@ -16,12 +16,28 @@ class EmojiPickerWidget extends StatelessWidget {
         checkPlatformCompatibility: true,
         emojiViewConfig: EmojiViewConfig(
           emojiSizeMax: 28 * (foundation.defaultTargetPlatform == TargetPlatform.iOS ? 1.20 : 1.0),
+          backgroundColor: Colors.black, // Background color for emoji view
         ),
         swapCategoryAndBottomBar: false,
-        skinToneConfig: const SkinToneConfig(),
-        categoryViewConfig: const CategoryViewConfig(),
-        bottomActionBarConfig: const BottomActionBarConfig(),
-        searchViewConfig: const SearchViewConfig(),
+        skinToneConfig: const SkinToneConfig(
+          enabled: true,
+          indicatorColor: Colors.white,
+          dialogBackgroundColor: Colors.black, // Background color for skin tone selector
+        ),
+        categoryViewConfig: const CategoryViewConfig(
+          backgroundColor: Colors.black, // Background color for category view
+          iconColor: Colors.white, // Icon color for category icons
+          iconColorSelected: Colors.blue, // Color for selected category icon
+        ),
+        bottomActionBarConfig: const BottomActionBarConfig(
+          backgroundColor: Colors.black, // Background color for bottom action bar
+          buttonIconColor: Colors.white, // Icon color for bottom action bar icons
+        ),
+        searchViewConfig: const SearchViewConfig(
+          backgroundColor: Colors.black, // Background color for search view
+          buttonIconColor: Colors.white, // Icon color for search view icons
+          buttonColor: Colors.white, // Text color for search input
+        ),
       ),
     );
   }
--- lib/ui/widgets/input_field.dart
+++ lib/ui/widgets/input_field.dart
@@ -8,96 +8,127 @@ import 'package:gemini_demo/ui/widgets/common_input_field_buttons.dart';
 import 'package:gemini_demo/ui/widgets/common_text_form_field.dart';
 import 'package:gemini_demo/ui/widgets/pop_up_menu.dart';
 
-class InputFieldWidget extends StatelessWidget {
+class InputFieldWidget extends StatefulWidget {
   const InputFieldWidget({super.key, this.model});
 
   final ChatViewModel? model;
 
   @override
+  _InputFieldWidgetState createState() => _InputFieldWidgetState();
+}
+
+class _InputFieldWidgetState extends State<InputFieldWidget> {
+  bool canPop = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 10, left: 15, right: 15, top: 10),
-      child: Row(
-        children: [
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(0),
-              decoration: BoxDecoration(
-                color: ColorConstants.grey3D4354,
-                borderRadius: BorderRadius.circular(25.0),
+    return PopScope(
+      canPop: canPop,
+      onPopInvoked: (bool value) {
+        if (widget.model?.showEmoji == false) {
+          setState(() {
+            canPop = !value;
+          });
+
+          if (canPop) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text("Click once more to go back"),
+                duration: Duration(milliseconds: 1500),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  model?.imageFile != null
-                      ? Container(
-                          padding: const EdgeInsets.only(left: 22),
-                          child: TextFormFieldWidget(
-                            model: model,
-                          ),
+            );
+          }
+        } else {
+          FocusScope.of(context).unfocus();
+          setState(() {
+            widget.model?.setShowEmoji = false;
+          });
+        }
+      },
+      child: Container(
+        margin: const EdgeInsets.only(bottom: 10, left: 15, right: 15, top: 10),
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(0),
+                decoration: BoxDecoration(
+                  color: ColorConstants.grey3D4354,
+                  borderRadius: BorderRadius.circular(25.0),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    widget.model?.imageFile != null
+                        ? Container(
+                            padding: const EdgeInsets.only(left: 22),
+                            child: TextFormFieldWidget(
+                              model: widget.model,
+                            ),
+                          )
+                        : const SizedBox(),
+                    widget.model?.imageFile != null
+                        ? Container(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: Image(
+                              height: 200,
+                              width: 200,
+                              image: FileImage(widget.model?.imageFile ?? File(ImageConstants.demo)),
+                            ),
+                          )
+                        : const SizedBox(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        InputFieldButton(
+                          icon: Icons.emoji_emotions,
+                          onpressed: () {
+                            FocusScope.of(context).unfocus();
+                            widget.model?.setShowEmoji = true;
+                          },
+                        ),
+                        widget.model?.imageFile == null
+                            ? Flexible(
+                                child: TextFormFieldWidget(
+                                model: widget.model,
+                              ))
+                            : const SizedBox(),
+                        InputFieldButton(
+                          icon: Icons.attach_file,
+                          onpressed: () {
+                            widget.model?.setShowEmoji = false;
+                            showModalBottomSheet(
+                                backgroundColor: ColorConstants.transparent,
+                                context: context,
+                                isScrollControlled: true,
+                                builder: (builder) => Padding(
+                                    padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+                                    child: PopUpWidget(
+                                      model: widget.model,
+                                    )));
+                          },
+                        ),
+                        InputFieldButton(
+                          icon: Icons.send,
+                          onpressed: () {
+                            if (widget.model?.imageFile != null && widget.model!.messageController.text.toString().isEmpty) {
+                              widget.model?.getTextAndImageInfo();
+                            }
+                            if (widget.model?.messageController.text.toString().isNotEmpty ?? false) {
+                              widget.model?.getTextAndImageInfo();
+                              widget.model?.messageController.clear();
+                            }
+                          },
                         )
-                      : const SizedBox(),
-                  model?.imageFile != null
-                      ? Container(
-                          padding: const EdgeInsets.only(top: 5),
-                          child: Image(
-                            height: 200,
-                            width: 200,
-                            image: FileImage(model?.imageFile ?? File(ImageConstants.demo)),
-                          ),
-                        )
-                      : const SizedBox(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      InputFieldButton(
-                        icon: Icons.emoji_emotions,
-                        onpressed: () {
-                          FocusScope.of(context).unfocus();
-                          model?.setShowEmoji = true;
-                        },
-                      ),
-                      model?.imageFile == null
-                          ? Flexible(
-                              child: TextFormFieldWidget(
-                              model: model,
-                            ))
-                          : const SizedBox(),
-                      InputFieldButton(
-                        icon: Icons.attach_file,
-                        onpressed: () {
-                          model?.setShowEmoji = false;
-                          showModalBottomSheet(
-                              backgroundColor: ColorConstants.transparent,
-                              context: context,
-                              isScrollControlled: true,
-                              builder: (builder) => Padding(
-                                  padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-                                  child: PopUpWidget(
-                                    model: model,
-                                  )));
-                        },
-                      ),
-                      InputFieldButton(
-                        icon: Icons.send,
-                        onpressed: () {
-                          if (model?.imageFile != null && model!.messageController.text.toString().isEmpty) {
-                            model?.getTextAndImageInfo();
-                          }
-                          if (model?.messageController.text.toString().isNotEmpty ?? false) {
-                            model?.getTextAndImageInfo();
-                            model?.messageController.clear();
-                          }
-                        },
-                      )
-                    ],
-                  )
-                ],
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
